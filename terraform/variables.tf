02d13bda4d384dc4e00506114cbbfca59d30ee58
Yusik Kim <yusik.kim@outlook.com>
2021-08-09 19:54:12 -0700

    Finished networking, subnet, and ec2 instance creation.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,5 +2,11 @@ variable "tags" {
   type = map
 }
 
+variable rig_instance_size {}
+variable rig_disk_size {}
+
+variable my_ip {}
+
+## AWS SECRETS
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
